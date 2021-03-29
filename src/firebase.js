5b14f320e223d2478fc3529e0c0a055df20e9297
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-29 20:58:55 +0100

    Added new FIREBASE

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD82ez9AOh7jiYUa18jZLjYaohr9kQtOnI",
-  authDomain: "project-a74c2.firebaseapp.com",
-  projectId: "project-a74c2",
-  storageBucket: "project-a74c2.appspot.com",
-  messagingSenderId: "135373350904",
-  appId: "1:135373350904:web:64edcfd6c5e13cd1dc180d",
-  measurementId: "G-K201SM4F07",
+  apiKey: "AIzaSyD0DQ7rXo146mnKhquLDA3OCaA6z3hw7nk",
+  authDomain: "web-app-740da.firebaseapp.com",
+  projectId: "web-app-740da",
+  storageBucket: "web-app-740da.appspot.com",
+  messagingSenderId: "751084819158",
+  appId: "1:751084819158:web:7569ee06244c1207f72f77",
+  measurementId: "G-EHBK2F5N1C",
 };
